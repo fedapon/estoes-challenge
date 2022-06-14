f0f94e29d9f2ec9c871fdba4456efb18d39c5415
fedapon <fedapon@gmail.com>
2022-06-14 20:01:45 -0300

    project routes created and binded to /api/projects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv"
 import express from "express"
+import projectRoutes from "./routes/project.routes.js"
 
 const app = express()
 dotenv.config()
 const PORT = process.env.PORT | 3000
+
+app.use("/api/projects", projectRoutes)
 
 app.listen(PORT, function () {
     console.log(`Server started on port ${PORT}`)
