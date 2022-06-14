976181aa796de4aadaa2413d8654e8c8908501f7
fedapon <fedapon@gmail.com>
2022-06-14 20:02:16 -0300

    dummy controller created

---
+++ src/controllers/project/project.controller.js
@@ -0,0 +1,19 @@
+export async function getAllProjects(req, res) {
+    return res.status(200).json({ message: "OK" })
+}
+
+export async function getProject(id) {
+    return true
+}
+
+export async function createProject() {
+    return true
+}
+
+export async function updateProject() {
+    return true
+}
+
+export async function deleteProject() {
+    return true
+}
