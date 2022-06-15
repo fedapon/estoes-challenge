b1666605836c34c50a2c6470a5459d2cfc674467
fedapon <fedapon@gmail.com>
2022-06-15 00:39:37 -0300

    initial GET method

--- src/controllers/project/project.controller.js
+++ src/controllers/project/project.controller.js
@@ -1,11 +1,28 @@
 const db = require("../../models")
 
 async function getAllProjects(req, res) {
-    return res.status(200).json({ message: "OK" })
+    try {
+        const projects = await db.Project.findAll()
+        return res
+            .status(200)
+            .json({ message: "list of all projects", projects })
+    } catch (err) {
+        return res.status(500).json({
+            message: "internal server error",
+        })
+    }
 }
 
-async function getProject(id) {
-    return true
+async function getProject(req, res) {
+    try {
+        const { id } = req.params
+        const project = await db.Project.findByPk(id)
+        return res.status(200).json({ message: "list one project", project })
+    } catch (err) {
+        return res.status(500).json({
+            message: "internal server error",
+        })
+    }
 }
 
 async function createProject() {
