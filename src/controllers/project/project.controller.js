4f8c0f0db7239448d21045346508a9265d88c423
fedapon <fedapon@gmail.com>
2022-06-15 09:34:02 -0300

    added assignedTo and projectManager to GET methods

--- src/controllers/project/project.controller.js
+++ src/controllers/project/project.controller.js
@@ -2,13 +2,28 @@ const db = require("../../models")
 
 async function getAllProjects(req, res) {
     try {
-        const projects = await db.Project.findAll()
+        const projects = await db.Project.findAll({
+            attributes: { exclude: ["project_manager_id"] },
+            include: [
+                {
+                    model: db.User,
+                    as: "projectManager",
+                    attributes: ["id", "firstName", "lastName"],
+                },
+                {
+                    model: db.User,
+                    as: "assignedTo",
+                    attributes: ["id", "firstName", "lastName"],
+                },
+            ],
+        })
         return res
             .status(200)
             .json({ message: "list of all projects", projects })
     } catch (err) {
         return res.status(500).json({
             message: "internal server error",
+            error: err.message,
         })
     }
 }
@@ -16,7 +31,21 @@ async function getAllProjects(req, res) {
 async function getProject(req, res) {
     try {
         const { id } = req.params
-        const project = await db.Project.findByPk(id)
+        const project = await db.Project.findByPk(id, {
+            attributes: { exclude: ["project_manager_id"] },
+            include: [
+                {
+                    model: db.User,
+                    as: "projectManager",
+                    attributes: ["id", "firstName", "lastName"],
+                },
+                {
+                    model: db.User,
+                    as: "assignedTo",
+                    attributes: ["id", "firstName", "lastName"],
+                },
+            ],
+        })
         return res.status(200).json({ message: "list one project", project })
     } catch (err) {
         return res.status(500).json({
