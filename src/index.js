8db35a1414765113ea257bc9e0f2570d3fd42299
fedapon <fedapon@gmail.com>
2022-06-17 10:49:05 -0300

    added users routes

--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@ const dotenv = require("dotenv")
 const express = require("express")
 const cors = require("cors")
 const projectRoutes = require("./routes/project.routes")
+const userRoutes = require("./routes/user.routes")
 
 const app = express()
 dotenv.config()
 const PORT = process.env.PORT || 3000
-
-if (process.argv[2] === "createDatabase") {
-    async function updateDatabase() {
-        const db = require("./models")
-        await db.sequelize.authenticate()
-        await db.sequelize.sync({ force: true })
-        console.log("Database created")
-    }
-    updateDatabase()
-}
 
 //swagger
 const { serve, setup } = require("swagger-ui-express")
@@ -30,6 +21,7 @@ app.use(express.urlencoded({ extended: false }))
 //routes
 app.use("/api/docs", serve, setup(swaggerJSDocs))
 app.use("/api/projects", projectRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, function () {
     console.log(`Server started on port ${PORT}`)
